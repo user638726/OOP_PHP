bf2ab2531647d20669d2258220d45509fe5347fc
user638726 <lienjackson35@gmail.com>
2024-11-26 11:52:22 +0800

    增加function改建的方式

---
+++ db.php
@@ -0,0 +1,30 @@
+<?php
+   class DB{
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db99";
+     protected $pdo;
+     protected $table;
+     
+     function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+     }
+    function all(){
+
+        return $this->q("SELECT* FROM $this->table");
+    }
+     function q($sql){
+        //echo $sql;
+       return $this->pdo->query($sql)->fetchAll();
+    }
+}
+function dd($array){
+       echo "<pre>";
+       print_r($array);
+       echo "<pre>";
+     } 
+       $DEPT=new DB('imgs');
+       //$dept=$DEPT->q("SELECT * FROM imgs");
+       $dept=$DEPT->all();
+       dd($dept);
+   
+?>
